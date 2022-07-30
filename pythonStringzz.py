73838dabd2994dd26b982328ecbe9dbf6329571c
JosephMcE890 <jcm1664@rit.edu>
2022-07-30 13:06:50 -0400

    Learning about indexing

--- pythonStringzz.py
+++ pythonStringzz.py
@@ -1,9 +1,15 @@
+#Learning about indexing
 myString = "Hello World"
 
-print(myString[0]) #Learning about indexing
-print(myString[1])
-print(myString[2])
-print(myString[3])
-print(myString[4])
-print(myString[5])
-print(myString[6])
+print(myString[0]) #H  
+print(myString[1])  #E
+print(myString[2])#L
+print(myString[3])#L
+print(myString[4])#0
+print(myString[5])#W
+print(myString[6])#O
+
+print('\n'+myString[-1])#D
+print(myString[-2])#L
+print(myString[-3])#R
+print(myString[-4])#O
