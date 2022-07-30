d2751dbf072cefc04748def5ef51b66518cf1b3a
JosephMcE890 <jcm1664@rit.edu>
2022-07-30 13:38:33 -0400

    Learning More!

--- pythonStringzz.py
+++ pythonStringzz.py
@@ -1,8 +1,8 @@
 #Learning about indexing
 myString = "Hello World"
 
-print(myString[0]) #H  
-print(myString[1])  #E
+print(myString[0])#H  
+print(myString[1])#E
 print(myString[2])#L
 print(myString[3])#L
 print(myString[4])#0
@@ -30,3 +30,21 @@ print(myString[1:3]) #bc
 print(myString[::2]) #jumps two steps
 print(myString[::3]) #jumps three steps
 print(myString[::-1]) #prints in reverse
+
+#Learning about inmutation or catcanation
+myString = 'Hello World' 
+print(myString)
+myString = myString + ' It is a beautiful day outside' #Inmutation!
+print(myString)
+
+#Learning about basic methods
+myString = 'Hello World'
+myString = myString.upper() # makes everything in the string uppercase
+print(myString)
+myString = myString.lower() #makes everything in the string lowercase
+print(myString)
+
+myString = 'Hello World'
+myString = myString.split('l') #splits string into a list based on what you put in the parentthesis
+print(myString)
+
