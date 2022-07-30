e0caf952e37f75e10ece2d6ab5f56d68dfb862ba
JosephMcE890 <jcm1664@rit.edu>
2022-07-30 13:24:33 -0400

    Learning...

--- pythonStringzz.py
+++ pythonStringzz.py
@@ -13,3 +13,20 @@ print('\n'+myString[-1])#D
 print(myString[-2])#L
 print(myString[-3])#R
 print(myString[-4])#O
+
+#could also index like this
+print('\n\nHelloWorld'[4]+'\n')
+
+
+#Learning about splicing
+myString = "abcdefghijklmnop"
+
+print(myString[2:]) #cdefghijklmnop
+print(myString[:3]) #adc *note- :3 = up to d but not included
+print(myString[3:6]) #def
+print(myString[1:3]) #bc
+
+#Learning about stepsize
+print(myString[::2]) #jumps two steps
+print(myString[::3]) #jumps three steps
+print(myString[::-1]) #prints in reverse
