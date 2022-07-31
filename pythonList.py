02cf4cbed865b47a468c1cbf60f7aa098aac3bf7
JosephMcE890 <jcm1664@rit.edu>
2022-07-30 23:45:51 -0400

    List learning

---
+++ pythonList.py
@@ -0,0 +1,19 @@
+#Learning about list
+my_list = ['strings', 100,23.2]
+print(my_list) #strings 100 23.2
+print(len(my_list)) #3
+
+my_list = ["one","two","three"]
+ #indexing list
+print(my_list[0])#one
+#slicing list
+print(my_list[0:2])#1,2
+
+#immutation
+another_list = ['four,five']
+my_list = my_list + another_list
+print(my_list) # one two three four five
+
+#adding new element to list
+my_list.append('six') #append = adds item to end of the list
+
