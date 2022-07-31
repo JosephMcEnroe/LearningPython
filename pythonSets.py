c2725dc01f451de6426b5eeff23dd762e052ee33
JosephMcE890 <jcm1664@rit.edu>
2022-07-31 12:28:56 -0400

    Sets!

---
+++ pythonSets.py
@@ -0,0 +1,12 @@
+#Learning about Sets
+#Sets are unordered collections of unique elements
+#This means that there could only be one repersentive of the same object
+
+mySet = set ()
+
+mySet.add(1)#add 1 to the set
+print(mySet)#1
+mySet.add(2)#adds 2 to the set(now we can't add 2 and 1 again)
+print(mySet)# 1, 2
+
+
