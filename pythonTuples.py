821f8c0049c7cd42d848aaa1d951f8950c9dd7c1
JosephMcE890 <jcm1664@rit.edu>
2022-07-31 12:21:25 -0400

    Tuples!

---
+++ pythonTuples.py
@@ -0,0 +1,14 @@
+#learning about tuples
+t = (1,2,3) #tuple
+
+print(t)#1,2,3
+print(len(t))#checks length of tuple
+
+t = (1,'two',3)# can also hold many diffrent data types
+print(t)
+
+t = ('a','a','b')
+print(t.count('a'))#counts how many times 'a' shows us
+print(t.index('a'))#shows where 'a' first shows up)
+
+#***IMPORTANT u can't change tuples the way u can change list (tuples are immutable)
