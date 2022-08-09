4d17ae5ef50d2179b0f8cfec22ea830cdcb1b5eb
JosephMcE890 <jcm1664@rit.edu>
2022-08-08 23:52:38 -0400

    MORE FUNCTIONS

--- pythonFunctions.py
+++ pythonFunctions.py
@@ -9,6 +9,7 @@
 #       Code the method is in
 
 from distutils.command import check
+from doctest import Example
 
 
 def say_hello():
@@ -62,3 +63,75 @@ def check_even_list(num_List):
 
 jeff = check_even_list([1,3,5,4,6])
 print(jeff)
+
+##Tuple Functions
+stock_prices = [('APPL',200),('GOOG',400),('MSFT',100)]
+for item in stock_prices:
+    print(item)
+for ticker, price in stock_prices:
+    print(price+(0.1*price))
+
+work_hours = [('Abby',100),('Billy',400),('Cassie',800)]
+
+def employee_check(work_hours):
+    
+    #returns employee with the highest amount of hours
+    current_max = 0
+    employee_of_month = ''
+
+    for employee, hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+
+    #return
+    return(employee_of_month,current_max)
+
+name,hours = employee_check(work_hours)
+
+print(name)
+print(hours)
+
+#interactions
+example = [1,2,3,4,5,6,7,8]
+
+from random import sample, shuffle
+
+shuffle(example)
+
+print(example)
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+result = shuffle_list(example)
+
+print(result) 
+
+mylist = ['','0',' ']
+
+def player_guess():
+    guess = ''
+
+    while guess not in ['0','1','2']:
+        guess = input("Pick a number: 0,1 or 2")
+    
+    return int(guess)
+
+
+
+def check_guess(mylist,guess):
+    if mylist[guess] == '0':
+        print("Correct!")
+    else: 
+        print("Wrong guess!")
+        print(mylist)
+        
+mylist = shuffle_list(mylist)
+guess = player_guess()
+check_guess(mylist,guess)
+
+
