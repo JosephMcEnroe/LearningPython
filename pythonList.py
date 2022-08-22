9eeae9490a4f8347a1706b9533b6196617480d27
JosephMcE890 <jcm1664@rit.edu>
2022-08-22 17:26:44 -0400

    List Methods

--- pythonList.py
+++ pythonList.py
@@ -6,8 +6,11 @@ print(len(my_list)) #3
 my_list = ["one","two","three"]
  #indexing list
 print(my_list[0])#one
+
 #slicing list
-print(my_list[0:2])#1,2
+print(my_list[0:2])#one, two
+print(my_list[:-1]) #one, two if the value is at the right side of the colon, 
+print(my_list[-1:]) #one, two
 
 #immutation
 another_list = ['four,five']
@@ -18,7 +21,8 @@ print(my_list) # one two three four five
 my_list.append('six') #append = adds item to end of the list
 print(my_list)#one two three four five six
 
-#deleting end of the list
+#pop method
+#can delete values at any part of the list
 popped_item = my_list.pop()
 print(my_list)#one two three four five 
 print(popped_item)
@@ -42,6 +46,17 @@ new_list = ["Apples","Pineapple","Oranges"]
 new_list.insert(-1,"banana")#insert banana at the end of the list
 print(new_list)
 
+#creating list using range ||  variable = range(how big you want your list)
+number_list = range(9)
+print(list(number_list)) #0, 1,2,3,4,5,6,7,8
+print(number_list)# range(0,9)
+
+number_list = range(2,9)
+print(list(number_list))#2,3,4,5,6,7,8
 
 
-
+#Count Method
+#counts about how many times said value shows up in a list
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+votes_for_jake = votes.count("Jake")
+print(votes_for_jake)
